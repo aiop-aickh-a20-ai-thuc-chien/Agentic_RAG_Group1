df687055aaa8cfda177dacb3d9d1cc4bb3a10d4f
Nguyen Tien Dung <dungnguyentien138@gmail.com>
2026-06-01 17:03:24 +0700

    Fix init.py 2

--- src/agentic_rag/ingestion/url/__init__.py
+++ src/agentic_rag/ingestion/url/__init__.py
@@ -1,22 +1,25 @@
-"""URL ingestion package."""
+# src/agentic_rag/ingestion/url/__init__.py
 
-from .loader import load_url_chunks
-# Assuming chunking classes live in a chunking module:
 from .chunking import (
-    GeminiChunkingClient,
-    LLMChunkingConfig,
-    ModelChunkingReport,
-    ModelChunkingStrategy,
-    OpenAIChunkingClient,
-    compare_model_chunking
+    TextChunkingStrategy,
+    build_chunks,
+    split_markdown,
+    build_chunk_id,
 )
+# Assuming you have a loader.py file next to this chunking file:
+from .loader import load_url_chunks 
+
+# If your tests are explicitly looking for old functions, 
+# point them to the new unified function as an alias to prevent breaking changes:
+load_html_chunks = load_url_chunks
+load_text_chunks = load_url_chunks
 
 __all__ = [
+    "TextChunkingStrategy",
+    "build_chunks",
+    "split_markdown",
+    "build_chunk_id",
     "load_url_chunks",
-    "GeminiChunkingClient",
-    "LLMChunkingConfig",
-    "ModelChunkingReport",
-    "ModelChunkingStrategy",
-    "OpenAIChunkingClient",
-    "compare_model_chunking",
+    "load_html_chunks",
+    "load_text_chunks",
 ]
