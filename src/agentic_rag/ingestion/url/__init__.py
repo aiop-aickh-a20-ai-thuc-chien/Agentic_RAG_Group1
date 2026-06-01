f108c284200520dc0c7d1f9ac2402cbe822aaa56
Nguyen Tien Dung <dungnguyentien138@gmail.com>
2026-06-01 17:04:39 +0700

    Revert init.py

--- src/agentic_rag/ingestion/url/__init__.py
+++ src/agentic_rag/ingestion/url/__init__.py
@@ -1,25 +1,5 @@
-# src/agentic_rag/ingestion/url/__init__.py
+"""URL ingestion package."""
 
-from .chunking import (
-    TextChunkingStrategy,
-    build_chunks,
-    split_markdown,
-    build_chunk_id,
-)
-# Assuming you have a loader.py file next to this chunking file:
-from .loader import load_url_chunks 
+from agentic_rag.ingestion.url.loader import load_url_chunks
 
-# If your tests are explicitly looking for old functions, 
-# point them to the new unified function as an alias to prevent breaking changes:
-load_html_chunks = load_url_chunks
-load_text_chunks = load_url_chunks
-
-__all__ = [
-    "TextChunkingStrategy",
-    "build_chunks",
-    "split_markdown",
-    "build_chunk_id",
-    "load_url_chunks",
-    "load_html_chunks",
-    "load_text_chunks",
-]
+__all__ = ["load_url_chunks"]
