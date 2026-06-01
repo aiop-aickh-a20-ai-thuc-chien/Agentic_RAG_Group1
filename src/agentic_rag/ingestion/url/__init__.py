eee13885e75034be35015231de29288e7354dea9
Nguyen Tien Dung <dungnguyentien138@gmail.com>
2026-06-01 16:50:30 +0700

    chore: sync ingestion folder with main to resolve conflicts

--- src/agentic_rag/ingestion/url/__init__.py
+++ src/agentic_rag/ingestion/url/__init__.py
@@ -1,23 +1,5 @@
 """URL ingestion package."""
 
-from agentic_rag.ingestion.url.loader import load_html_chunks, load_text_chunks, load_url_chunks
-from agentic_rag.ingestion.url.model_chunking import (
-    GeminiChunkingClient,
-    LLMChunkingConfig,
-    ModelChunkingReport,
-    ModelChunkingStrategy,
-    OpenAIChunkingClient,
-    compare_model_chunking,
-)
+from agentic_rag.ingestion.url.loader import load_url_chunks
 
-__all__ = [
-    "GeminiChunkingClient",
-    "LLMChunkingConfig",
-    "ModelChunkingReport",
-    "ModelChunkingStrategy",
-    "OpenAIChunkingClient",
-    "compare_model_chunking",
-    "load_html_chunks",
-    "load_text_chunks",
-    "load_url_chunks",
-]
+__all__ = ["load_url_chunks"]
